731412200c25ef43fc36a219c27f79d6d305d4bd
therahuljhacom <rahuljha.digilantern@gmail.com>
2024-07-08 16:42:38 +0530

    updated

--- src/components/homepage/MainSection/MainSection.tsx
+++ src/components/homepage/MainSection/MainSection.tsx
@@ -6,7 +6,7 @@ const MainSection = () => {
             <section className={styles["main-section"]}>
                 <div className="wrapper">
                     <div className={styles["banner-content"]}>
-                        <div className={`main-heading ${styles["ban-heading"]}`}>Automate Interactions, Personalize Transactions and Customer Services with Conversational AI</div>
+                        <div className={`main-heading ${styles["ban-heading"]}`}>Hello Rahul Interactions, Personalize Transactions and Customer Services with Conversational AI</div>
                         <div className={`sub-heading ${styles["ban-sub-heading"]}`}>
                             At ThinkChat, our solution foundation lies in the collaboration between human insight and AI capabilities. Our experts create the most advanced strategies that are unique, best, and effective. By blending your expert vision with our AI&apos;s extensive data analysis and creativity.
                         </div>
